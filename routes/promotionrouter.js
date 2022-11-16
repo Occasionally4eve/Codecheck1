2f4602ad6b6568c3a8c2d6d1fea0c569a83a2efd
Terrina Taylor <sunniebilly@yahoo.com>
2022-11-15 20:45:29 -0500

    Express Session part1

---
+++ routes/promotionrouter.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const promotionRouter = express.Router();
+
+const Promotion = require("../models/promotion");
+
+promotionRouter
+  .route("/")
+  .get((req, res, next) => {
+    Promotion.find()
+      .then((paromotion) => res.status(200).json(paromotion))
+      .catch((err) => next(err));
+  })
+  .post((req, res, next) => {
+    Promotion.create(req.body)
+      .then((promotion) => res.status(200).json(promotion))
+      .catch((err) => next(err));
+  })
+  .delete((req, res, next) => {
+    Promotion.deleteMany()
+      .then((paromotion) => res.status(200).json(paromotion))
+      .catch((err) => next(err));
+  });
+
+promotionRouter
+  .route("/:promotionId")
+  .get((req, res, next) => {
+    Promotion.findById(req.params.promotionId)
+      .then((promotion) => res.status(200).json(promotion))
+      .catch((err) => next(err));
+  })
+  .put((req, res, next) => {
+    Promotion.findByIdAndUpdate(req.params.promotionId),
+      {
+        $set: req.body,
+      },
+      { new: true }
+        .then((promotion) => res.status(200).json(promotion))
+        .catch((err) => next(err));
+  })
+  .delete((req, res, next) => {
+    Promotion.findByIdAndDelete(req.params.promotionId)
+      .then((promotion) => res.status(200).json(promotion))
+      .catch((err) => next(err));
+  });
+
+module.exports = promotionRouter;
